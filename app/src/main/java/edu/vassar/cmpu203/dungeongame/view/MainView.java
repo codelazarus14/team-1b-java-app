3193fc6175c88515661bd64f0ffd7118f0cb2db6
Sam Catterall <scatterall@vassar.edu>
2021-11-10 16:21:53 -0500

    Created MainView interface and template for MainView.java

---
+++ app/src/main/java/edu/vassar/cmpu203/dungeongame/view/MainView.java
@@ -0,0 +1,18 @@
+package edu.vassar.cmpu203.dungeongame.view;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public class MainView implements IMainView{
+
+    @Override
+    public View getRootView() {
+        return null;
+    }
+
+    @Override
+    public void displayFragment(Fragment fragment) {
+
+    }
+}
