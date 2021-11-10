4ba68056182ca4f9c13761964df1fb847395d813
Sam Catterall <scatterall@vassar.edu>
2021-11-10 16:33:45 -0500

    Finished setting up MainView.java and put a fragmentContainerView in main.xml

--- app/src/main/java/edu/vassar/cmpu203/dungeongame/view/MainView.java
+++ app/src/main/java/edu/vassar/cmpu203/dungeongame/view/MainView.java
@@ -3,16 +3,31 @@ package edu.vassar.cmpu203.dungeongame.view;
 import android.view.View;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import edu.vassar.cmpu203.dungeongame.databinding.MainBinding;
 
 public class MainView implements IMainView{
 
+    private FragmentActivity activity;
+    private MainBinding binding;
+
+    public MainView(FragmentActivity activity) {
+        this.activity = activity;
+        this.binding = MainBinding.inflate(activity.getLayoutInflater());
+    }
+
     @Override
     public View getRootView() {
-        return null;
+        return this.binding.getRoot();
     }
 
     @Override
     public void displayFragment(Fragment fragment) {
 
+        this.activity.getSupportFragmentManager()
+                .beginTransaction()
+                    .replace(this.binding.fragmentContainerView.getId(), fragment)
+                    .commitNow();
     }
 }
