c573d71909aac29bb32814b6e4068e59e04f20e9
Sam Catterall <scatterall@vassar.edu>
2021-11-16 11:55:44 -0500

    Wrote sample test in PlayGameTest to demonstrate how we should use instrumented tests

--- app/src/androidTest/java/edu/vassar/cmpu203/dungeongame/PlayGameTest.java
+++ app/src/androidTest/java/edu/vassar/cmpu203/dungeongame/PlayGameTest.java
@@ -1,9 +1,7 @@
 package edu.vassar.cmpu203.dungeongame;
 
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -13,14 +11,14 @@ import edu.vassar.cmpu203.dungeongame.controller.ControllerActivity;
 
 public class PlayGameTest {
     @org.junit.Rule
-    ActivityScenarioRule<ControllerActivity> activityRule =
-            new ActivityScenarioRule<ControllerActivity>(ControllerActivity.class);
+    public ActivityScenarioRule<ControllerActivity> activityRule =
+            new ActivityScenarioRule<>(ControllerActivity.class);
 
     @Test
     public void testPlayGame() {
         ViewInteraction mazeView = Espresso.onView(ViewMatchers.withId(R.id.mazeView));
-        mazeView.check(ViewAssertions.matches(ViewMatchers.withText(R.string.maze_view)));
 
-
+        // should fail - mazeText should no longer show default value
+        // mazeView.check(ViewAssertions.matches(ViewMatchers.withText(R.string.maze_view)));
     }
 }
