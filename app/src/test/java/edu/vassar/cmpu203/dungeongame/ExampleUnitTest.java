6bc02f32abc75f5a4059ea4400a09170d75fe7dc
Tian Hahn <chahn@vassar.edu>
2021-11-18 11:56:49 -0500

    Made Player getPos test, marked DisplayMazeConsole as being no longer in use

--- app/src/test/java/edu/vassar/cmpu203/dungeongame/ExampleUnitTest.java
+++ app/src/test/java/edu/vassar/cmpu203/dungeongame/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package edu.vassar.cmpu203.dungeongame;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
