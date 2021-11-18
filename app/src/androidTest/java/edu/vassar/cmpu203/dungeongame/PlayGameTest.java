949e8af77a07e21e49a5970d8ce541e83b2daab9
Sam Catterall <scatterall@vassar.edu>
2021-11-18 12:02:00 -0500

    Added MenuTest for testing menu + switching fragments

--- app/src/androidTest/java/edu/vassar/cmpu203/dungeongame/PlayGameTest.java
+++ app/src/androidTest/java/edu/vassar/cmpu203/dungeongame/PlayGameTest.java
@@ -2,6 +2,7 @@ package edu.vassar.cmpu203.dungeongame;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -16,9 +17,9 @@ public class PlayGameTest {
 
     @Test
     public void testPlayGame() {
-        ViewInteraction mazeView = Espresso.onView(ViewMatchers.withId(R.id.mazeView));
+        // press start and switch to maze view
+        ViewInteraction startButton = Espresso.onView(ViewMatchers.withId(R.id.startButton)).perform(ViewActions.click());
 
-        // should fail - mazeText should no longer show default value
-        // mazeView.check(ViewAssertions.matches(ViewMatchers.withText(R.string.maze_view)));
+        // press a movement button and check if toString was updated with new String instance?
     }
 }
